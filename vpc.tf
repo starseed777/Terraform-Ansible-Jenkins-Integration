71ac430a4e3ceee1cc78201cbe17344419488922
starseed777 <waress66@live.com>
2020-12-22 12:22:53 -0500

    testing webhook for automation

--- vpc.tf
+++ vpc.tf
@@ -6,3 +6,12 @@ resource "aws_vpc" "main_vpc" {
         Name = "Main-VPC-${terraform.workspace}"
     }
 }
+
+resource "aws_subnet" "public_subnet" {
+    cidr_block = "10.0.2.0/16"
+    vpc_id = aws_vpc.main_vpc.id
+
+    tags = {
+        Name = "PubSubnet1"
+    }
+}
