1fe4251dd229a33e438dcc0d0d61d1980bfab76e
starseed777 <waress66@live.com>
2020-12-21 23:18:34 -0500

    making a new s3 bucket

--- providers.tf
+++ providers.tf
@@ -5,7 +5,7 @@ provider "aws" {
 terraform {
     
     backend "s3" {
-        bucket = "bucketjawnt"
+        bucket = "jenkyjawnt"
         key = "terraform.tfstate"
         region = "us-east-1"
         dynamodb_table = "tfbackup"
