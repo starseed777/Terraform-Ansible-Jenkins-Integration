6f1158c88f0d40f355209ceef52de91725a95574
starseed777 <waress66@live.com>
2020-12-22 12:39:02 -0500

    lil typo fixed

--- vpc.tf
+++ vpc.tf
@@ -8,6 +8,6 @@ resource "aws_vpc" "main_vpc" {
 }
 
 resource "aws_subnet" "public_subnet" {
-    cidr_block = var.public_subnet
+    cidr_block = var.subnet_cidr
     vpc_id = aws_vpc.main_vpc.id
 }
